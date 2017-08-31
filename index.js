3f830d1a9506879bcf75af95d3e193baaa1dcbea
Denis Sorokin <denys-s@yit.co.il>
2017-08-31 09:32:09 +0300

    Changed remote repo

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-require('chromedriver');
+require('chromedriver')
+var promise = require('selenium-webdriver').promise
 var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
-var driver = new webdriver.Builder()
+    By = webdriver.By;
+
+let driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
-driver.get('http://yad4.co.il/');
+driver.get('http://yad4.co.il/')
 
-var promise = require('selenium-webdriver').promise;
 
-var pendingElements = driver.findElements(By.className('yd-ban-tit'))
+let pendingElements = driver.findElements(By.className('yd-ban-tit'))
 
-pendingElements.then(function (elements) {
-    var pendingHtml = elements.map(function (elem) {
-        return elem.getText();
-    });
+pendingElements.then(elements => {
+    let pendingHtml = elements.map(elem => {
+        return elem.getText()
+    })
 
-    promise.all(pendingHtml).then(function (allHtml) {
+    promise.all(pendingHtml).then(allHtml => {
         console.log(allHtml)
-    });
-});
+    })
+})
 
-driver.quit();
+driver.quit()
