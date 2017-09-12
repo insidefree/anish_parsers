0d8f99fe2a880cfb239c1c7788055010ee77956e
Denis Sorokin <denys-s@yit.co.il>
2017-09-12 15:21:40 +0300

    Added upload to storage

---
+++ tests/bucket.js
@@ -0,0 +1,32 @@
+const keyFilename="../anish_parser/src/config/fb_conf.json"; //replace this with api key file
+const projectId = "anish-6cd8e" //replace with your project id
+const bucketName = `${projectId}.appspot.com`;
+
+const mime = require('mime');
+const gcs = require('@google-cloud/storage')({
+    projectId,
+    keyFilename
+});
+
+const bucket = gcs.bucket(bucketName);
+
+const filePath = `./img/test.jpg`;
+const uploadTo = `images/test.jpg`;
+const fileMime = mime.lookup(filePath);
+
+bucket.upload(filePath, {
+    destination: uploadTo,
+    public: true,
+    metadata: { contentType: fileMime, cacheControl: "public, max-age=300" }
+}, function (err, file) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(createPublicFileURL(uploadTo));
+});
+
+function createPublicFileURL(storageName) {
+    return `http://storage.googleapis.com/${bucketName}/${encodeURIComponent(storageName)}`;
+
+}
--- tests/dwTest4.js
+++ tests/dwTest4.js
@@ -6,15 +6,38 @@ var Jimp = require("jimp")
 var storage = require('@google-cloud/storage');
 var fs = require('fs');
 
-const config = {
-    apiKey: "AIzaSyDHKja4zGoInK7D5PsL4MCHAYC2lmvGGPA",
-    authDomain: "anish-6cd8e.firebaseapp.com",
-    databaseURL: "https://anish-6cd8e.firebaseio.com",
-    projectId: "anish-6cd8e",
-    storageBucket: "anish-6cd8e.appspot.com",
-    messagingSenderId: "613075498242"
-}
+var admin = require("firebase-admin");
 
+var serviceAccount = require('../src/config/fb_conf.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://anish-6cd8e.firebaseio.com"
+});
+
+
+// var bucket = admin.storage().bucket
+
+// var gcs = storage({
+//     projectId: 'anish-6cd8e',
+//     keyFilename: '../src/config/fb_conf.json'
+//   });
+
+// // Create a new bucket. 
+// gcs.createBucket('my-new-bucket', function(err, bucket) {
+//     if (!err) {
+//         console.log('was successfully created')
+//       // "my-new-bucket" was successfully created. 
+//     }
+//   });
+// var bucket = gcs.bucket('my-new-bucket');
+// // Upload a local file to a new file to be created in your bucket. 
+// bucket.upload('../img/test.jpg', function(err, file) {
+//     if (!err) {
+//       // "zebra.jpg" is now in your bucket. 
+//     }
+//   });
+
+//  ***********************
 // cloudscraper.request({
 //     method: 'GET',
 //     url: 'http://www.letlive.org.il/wp-content/uploads/2017/04/mikel2-330x330.jpg',
@@ -26,42 +49,3 @@ const config = {
 //     })
 // });
 
-
-// Authenticating on a per-API-basis. You don't need to do this if you auth on a 
-// global basis (see Authentication section above). 
-
-var gcs = storage({
- projectId: 'anish-6cd8e',
- keyFilename: config
-});
-
-// Create a new bucket. 
-gcs.createBucket('my-new-bucket', function(err, bucket) {
- if (!err) {
-   // "my-new-bucket" was successfully created. 
- }
-});
-
-// Reference an existing bucket. 
-var bucket = gcs.bucket('my-existing-bucket');
-
-// Upload a local file to a new file to be created in your bucket. 
-bucket.upload('./img/test.jpg', function(err, file) {
- if (!err) {
-   // "zebra.jpg" is now in your bucket. 
- }
-});
-
-// Download a file from your bucket. 
-bucket.file('test.jpg').download({
- destination: './photos/zoo/test.jpg'
-}, function(err) {});
-
-// Streams are also supported for reading and writing files. 
-var remoteReadStream = bucket.file('test.jpg').createReadStream();
-var localWriteStream = fs.createWriteStream('./photos/zoo/test.jpg');
-remoteReadStream.pipe(localWriteStream);
-
-var localReadStream = fs.createReadStream('/photos/zoo/zebra.jpg');
-var remoteWriteStream = bucket.file('zebra.jpg').createWriteStream();
-localReadStream.pipe(remoteWriteStream);
