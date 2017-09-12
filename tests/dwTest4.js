7b8acadfa64e85cec599db9b477f7fd7bb4521c4
Denis Sorokin <denys-s@yit.co.il>
2017-09-12 13:38:08 +0300

    refactoring

--- tests/dwTest4.js
+++ tests/dwTest4.js
@@ -13,28 +13,13 @@ const config = {
     messagingSenderId: "613075498242"
 }
 
-// var firebaseApp = firebase.initializeApp(config)
-// var storageRef = firebase.storage().ref('images/file_name.jpg')
-
-
-
 cloudscraper.request({
     method: 'GET',
     url: 'http://www.letlive.org.il/wp-content/uploads/2017/04/mikel2-330x330.jpg',
     encoding: null,
 }, function (err, response, body) {
     console.log(response)
-    // var buf = new Buffer(body)
-    // console.log( new Buffer(body, "base64"))
-    // fs.writeFile("arghhhh.jpg", body.toString('binary'), function(err) {});
-    // var base64Image = new Buffer(body, 'binary').toString('base64');
-    // console.log(base64Image)
-    // var decodedImage = new Buffer(base64Image, 'base64').toString('binary');
-    // console.log(decodedImage)
-    // fs.writeFile('image_decoded.jpg', decodedImage, function(err) {});
     Jimp.read(body, function (err, image) {
-        // Jimp.MIME_JPEG;
         image.write( './tests/img/test.jpg', ()=> {console.log(err)} );
-        // fs.writeFile('image_decoded.jpg', image, function(err) {});
     })
 });
