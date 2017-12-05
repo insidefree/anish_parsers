fc70551dc446b9aa0afbbb575491004ac3d07fc2
Denis Sorokin <denys.sorokin@gmail.com>
2017-12-05 21:46:10 +0200

    Parse test

--- src/App.js
+++ src/App.js
@@ -3,23 +3,54 @@ var promise = require('selenium-webdriver').promise
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By;
 
-let driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
 
-driver.get('http://yad4.co.il/')
+class Parse {
+    constructor() {
+        this.links = {}
+        this.count = 1
+        this.driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .build()
+    }
+
+    getSetLinks(link, deep) {
+        this.driver.get(link)
+        let pendingElements = this.driver.findElements(By.tagName('a'))
+        pendingElements.then(elements => {
+            let pendingHtml = elements.map(elem => {
+                return elem.getAttribute("href")
+            })
+            promise.all(pendingHtml)
+                .then(allHtml => {
+                    this.links[this.count - 1] = new Set(allHtml)
+                    console.log(this.links)
+                })
+                // .then(() => this.getDeeper(deep))
+                .then(() => console.log(this.links))
+
+        })
+
+        this.closeConenction()
+    }
+
+    getDeeper(deep) {
+        while (this.count < deep) {
+            this.links[this.count - 1].forEach(link => {
+                this.getSetLinks(link)
+            })
+            this.count++
+        }
+        this.count = 1
+    }
+
+    closeConenction() {
+        this.driver.quit()
+    }
+
+}
 
 
-let pendingElements = driver.findElements(By.className('yd-ban-tit'))
+let ynet = new Parse()
+ynet.getSetLinks('http://www.ynet.co.il/', 2)
 
-pendingElements.then(elements => {
-    let pendingHtml = elements.map(elem => {
-        return elem.getText()
-    })
 
-    promise.all(pendingHtml).then(allHtml => {
-        console.log(allHtml)
-    })
-})
-
-driver.quit()
